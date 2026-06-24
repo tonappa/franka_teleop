f0dd66e24d502d5a517a42d117c53fe1fb8dee71
SamueleBordini <bordinisamuele@gmail.com>
2026-06-24 16:47:04 +0200

    final ex

--- src/shared_autonomy/scripts/shared_autonomy.py
+++ src/shared_autonomy/scripts/shared_autonomy.py
@@ -16,6 +16,74 @@ OPT_SOL = False  # Set to True to use the optimal solution function
 
 ##############################################################################################################
 # Function to blend the autonomous and teleoperated poses with the blending factor
+def ex_blending_function(a, b, c):#
+
+    blended_pose = PoseStamped()
+
+    ##############################################################################
+    #                            Write here your code!                           #
+    #     Write the blanding function that takes in the teleoperation pose,      #        
+    #      autonomy pose, and blending factor and returns the blended pose       #
+    #               blending factor = 1 -----> teleoperated pose                 #
+    #               blending factor = 0 -----> autonomy pose                     #
+    ##############################################################################
+    
+   
+
+    blended_pose.pose.orientation = blending_function_orientation(teleop_pose, autonomy_pose, blending_factor).pose.orientation
+
+
+    return blended_pose
+###############################################################################################################
+
+
+
+
+##############################################################################################################
+# Function to create the linear function 
+def ex_optimal_solution(indipendent_variable, param_vec):
+
+    ##############################################################################
+    #                            Write here your code!                           #
+    #     Write the optimal solution function that takes in the independent      #        
+    #      variable and parameters and returns the optimal blending factor       #                           
+    ##############################################################################
+
+
+    return blending_factor
+###############################################################################################################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+##############################################################################################################
+# Function to blend the autonomous and teleoperated poses with the blending factor
 def blending_function(teleop_pose, autonomy_pose, blending_factor):#
 
     blended_pose = PoseStamped()
